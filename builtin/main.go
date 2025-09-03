5ea33387af38a54681f85d2afad53814ba84d7f9
Matthias Bertschy <matthias.bertschy@gmail.com>
2025-09-03 17:09:48 +0200

    initial commit
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

---
+++ builtin/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	gadgetregistry "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-registry"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/open/tracer"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/open/types"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/local"
+)
+
+func do() error {
+	runtime := local.New()
+	if err := runtime.Init(nil); err != nil {
+		return fmt.Errorf("runtime init: %w", err)
+	}
+	defer runtime.Close()
+
+	gadgetDesc := gadgetregistry.Get("trace", "open")
+	parser := gadgetDesc.Parser()
+	parser.SetEventCallback(func(ev any) {
+		event := ev.(*types.Event)
+		fmt.Printf("command %s (%d) opened %s\n", event.Comm, event.Pid, event.Path)
+	})
+
+	gadgetCtx := gadgetcontext.NewBuiltIn(
+		context.TODO(),
+		"",
+		runtime,
+		runtime.ParamDescs().ToParams(),
+		gadgetDesc,
+		gadgetDesc.ParamDescs().ToParams(),
+		[]string{},
+		params.Collection{},
+		parser,
+		logger.DefaultLogger(),
+		time.Duration(0),
+	)
+
+	if _, err := runtime.RunBuiltInGadget(gadgetCtx); err != nil {
+		return fmt.Errorf("running gadget: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := do(); err != nil {
+		fmt.Printf("Error running application: %s\n", err)
+		os.Exit(1)
+	}
+}
